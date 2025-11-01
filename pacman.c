60f47a0ffac6e3bcf49db54904d1eaaebfc2cde7
arifmhd007 <102906665+Arifmhd007@users.noreply.github.com>
2025-11-01 20:29:00 +0530

    Update pacman.c

--- pacman.c
+++ pacman.c
@@ -1,35 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <conio.h>     // For _kbhit(), _getch()
-#include <windows.h>   // For Sleep()
+#include <conio.h>
+#include <windows.h>
+#include <time.h>
+#include <math.h>
 
-#define MAP_ROWS 13
-#define MAP_COLS 20
+#define MAP_ROWS 30
+#define MAP_COLS 55
 
 int main() {
     char map[MAP_ROWS][MAP_COLS + 1] = {
-        "####################",
-        "#........#........#",
-        "#.####...#...####.#",
-        "#o#  #...#...#  #o#",
-        "#.####...#...####.#",
-        "#........ ........#",
-        "###.###.#####.###.#",
-        "#........#........#",
-        "#.####...#...####.#",
-        "#o#  #...#...#  #o#",
-        "#.####...#...####.#",
-        "#........P.......G#",
-        "####################"
+        "#######################################################",
+        "#.........................#.........................#",
+        "#.#####.#########.#######.#.#######.#########.#####.#",
+        "#o#   #.#       #.#     #.#.#     #.#       #.#   #o#",
+        "#.###.#.#.###.#.#.#.###.#.#.#.###.#.#.###.#.#.#.###.#",
+        "#.....#.#.....#.#.#.....#.#.#.....#.#.....#.#.#.....#",
+        "###.#.#.#######.#.#######.#.#######.#.#######.#.###.#",
+        "#...#.#........#.........#.#.........#........#.#...#",
+        "#.###.#########.#########.#.#########.#########.###.#",
+        "#.#...............................................#.#",
+        "#.#.#########.#########################.#########.#.#",
+        "#.#.#...........................................#.#.#",
+        "#.#.#.#########.###########.###########.#########.#.#",
+        "#.#.#.#.......#.#.........#.#.........#.#.......#.#.#",
+        "#.#.#.#.###.#.#.#.#######.#.#.#######.#.#.###.#.#.#.#",
+        "#.#.#.#.....#.#.#.......#.#.#.......#.#.#.....#.#.#.#",
+        "#.#.#.#####.#.#.#######.#.#.#.#######.#.#####.#.#.#.#",
+        "#.#.#.......#.#.........#.#.#.........#.#.......#.#.#",
+        "#.#.#########.###########.#.###########.#########.#.#",
+        "#.#...............................................#.#",
+        "#.#################################################.#",
+        "#...................................................#",
+        "#.#################################################.#",
+        "#.#...............................................#.#",
+        "#.#.###########.###############.###############.#.#.#",
+        "#.#...........#...............#.................#.#.#",
+        "#.###########.#.#############.#.###############.#.#.#",
+        "#.....................P.........................G...#",
+        "#######################################################"
     };
 
-    int pac_r = 11, pac_c = 9;
-    int ghost_r = 11, ghost_c = 17;
+    int pac_r = 27, pac_c = 22;
+    int ghost_r = 27, ghost_c = 47;
     int dr = 0, dc = 0;
+    int gdr = 0, gdc = -1;
     int score = 0;
     int running = 1;
+    int chase_timer = 0; 
 
-    // Hide cursor (Windows)
+    srand((unsigned)time(NULL));
+
     HANDLE hout = GetStdHandle(STD_OUTPUT_HANDLE);
     CONSOLE_CURSOR_INFO cursorInfo;
     GetConsoleCursorInfo(hout, &cursorInfo);
@@ -37,7 +58,7 @@ int main() {
     SetConsoleCursorInfo(hout, &cursorInfo);
 
     while (running) {
-        // Input
+
         if (_kbhit()) {
             char ch = _getch();
             if (ch == 'w') { dr = -1; dc = 0; }
@@ -47,7 +68,6 @@ int main() {
             else if (ch == 'q') { running = 0; }
         }
 
-        // Move pacman
         int nr = pac_r + dr;
         int nc = pac_c + dc;
         if (map[nr][nc] != '#') {
@@ -55,24 +75,82 @@ int main() {
             pac_c = nc;
         }
 
-        // Eat pellet
         if (map[pac_r][pac_c] == '.' || map[pac_r][pac_c] == 'o') {
             score++;
             map[pac_r][pac_c] = ' ';
         }
 
-        // Move ghost
-        if (map[ghost_r][ghost_c - 1] != '#') ghost_c--;
-        else ghost_c = 17;
+        double dist = sqrt((pac_r - ghost_r)*(pac_r - ghost_r) + (pac_c - ghost_c)*(pac_c - ghost_c));
 
-        // Check collision
+        if (dist < 4) {
+            chase_timer = 200; 
+        }
+
+        int ngr = ghost_r + gdr;
+        int ngc = ghost_c + gdc;
+        int dirs[4][2] = {{-1,0},{1,0},{0,-1},{0,1}};
+
+        if (chase_timer > 0) {
+
+            chase_timer--;
+            int best_r = ghost_r, best_c = ghost_c;
+            double best_dist = 1e9;
+            int best_dr = 0, best_dc = 0;
+
+            for (int i = 0; i < 4; i++) {
+                int tr = ghost_r + dirs[i][0];
+                int tc = ghost_c + dirs[i][1];
+                if (map[tr][tc] != '#') {
+                    double d = sqrt((pac_r - tr)*(pac_r - tr) + (pac_c - tc)*(pac_c - tc));
+                    if (d < best_dist) {
+                        best_dist = d;
+                        best_r = tr;
+                        best_c = tc;
+                        best_dr = dirs[i][0];
+                        best_dc = dirs[i][1];
+                    }
+                }
+            }
+            gdr = best_dr;
+            gdc = best_dc;
+            ngr = best_r;
+            ngc = best_c;
+        } else {
+
+            int blocked = (map[ngr][ngc] == '#');
+            int openCount = 0;
+            for (int i = 0; i < 4; i++) {
+                int tr = ghost_r + dirs[i][0];
+                int tc = ghost_c + dirs[i][1];
+                if (map[tr][tc] != '#') openCount++;
+            }
+
+            if (blocked || openCount > 2) {
+                int validDirFound = 0;
+                for (int tries = 0; tries < 10 && !validDirFound; tries++) {
+                    int d = rand() % 4;
+                    int tr = ghost_r + dirs[d][0];
+                    int tc = ghost_c + dirs[d][1];
+                    if (map[tr][tc] != '#' && !(dirs[d][0] == -gdr && dirs[d][1] == -gdc)) {
+                        gdr = dirs[d][0];
+                        gdc = dirs[d][1];
+                        validDirFound = 1;
+                    }
+                }
+                ngr = ghost_r + gdr;
+                ngc = ghost_c + gdc;
+            }
+        }
+
+        if (map[ngr][ngc] != '#') {
+            ghost_r = ngr;
+            ghost_c = ngc;
+        }
+
         if (pac_r == ghost_r && pac_c == ghost_c)
             running = 0;
 
-        // Move cursor to top-left instead of clearing
         printf("\033[H");
-
-        // Draw map
         for (int r = 0; r < MAP_ROWS; r++) {
             for (int c = 0; c < MAP_COLS; c++) {
                 if (r == pac_r && c == pac_c)
@@ -85,12 +163,14 @@ int main() {
             putchar('\n');
         }
 
-        printf("Score: %d  (WASD to move, Q to quit)\n", score);
+        if (chase_timer > 0)
+            printf("Score: %d  [GHOST CHASING! %d]\n", score, chase_timer / 50);
+        else
+            printf("Score: %d  (WASD to move, Q to quit)\n", score);
 
-        Sleep(100);
+        Sleep(20);
     }
 
-    // Show cursor again
     cursorInfo.bVisible = TRUE;
     SetConsoleCursorInfo(hout, &cursorInfo);
 
